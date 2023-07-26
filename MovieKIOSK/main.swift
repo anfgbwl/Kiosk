323811bf064c13fce8b614c9725cc890550e1b72
Soobean <saen5848@naver.com>
2023-07-26 15:10:20 +0900

    허수빈이 방금 수정함

--- MovieKIOSK/main.swift
+++ MovieKIOSK/main.swift
@@ -36,24 +36,49 @@ while choice != "0" {
     
     switch choice {
     case "1":
+        print("1번 선택: 영화 목록 조회")
+    case "2":
+        print("2번 선택: 영화 예매")
+    case "0":
+        print("0번 선택: 프로그램 종료")
+    default:
+        print(errorLine)
+        print("잘못된 입력입니다. 메뉴 번호를 똑바로 보고 다시 입력해주세요.")
+    }
+    // 유효성 검사(1) : 1,2,0 외 번호 입력 시 오류문 출력 후 재 안내/ 일단 완료
+    
+    
+    switch choice {
+    case "1":
+        print(line)
         print("예매 차트\n예매하실 영화의 번호를 입력해주세요")
         movieList.enumerated().forEach({ print("\($0.0+1). \($0.1.title)")})
-        let movieIndex = Int(readLine()!)!
-        let movie = movieList[movieIndex-1]
-        // 유효성 검사(1) : 1,2,0 외 번호 입력 시 오류문 출력 후 재 안내
+        var movieIndex: Int?
+        repeat {
+            if let input = readLine(), let index = Int(input), index >= 1 && index <= movieList.count {
+                movieIndex = index - 1
+            } else {
+                print(errorLine)
+                print("잘못된 입력입니다. 영화 번호를 다시 입력해주세요.")
+            }
+        } while movieIndex == nil
+        let movie = movieList[movieIndex!]
+        // 유효성 검사(2) : 1,2,3,4 외 번호 입력 시 오류문 출력 후 재 안내/ 일단 완료
+
         
 //        labeledStatements => while 문에 이름 달아서 해당 구문으로 돌아갈 수 있음
-        
+        print(line)
         print("예매하실 상영 시간의 번호를 입력해주세요")
         movie.timeTable.enumerated().forEach{ print("\($0.0+1). \($0.1.time) \($0.1.price) \($0.1.remainedSeat)/30") }
         let timeIndex = Int(readLine()!)!
         let time = movie.timeTable[timeIndex-1]
-        // 유효성 검사(2) : 1,2,3,4 외 번호 입력 시 오류문 출력 후 재 안내
-
+        
+        print(line)
         print("예매하실 인원 수를 입력해주세요")
         let headCount = Int(readLine()!)!
         // 유효성 검사(3) : 최대 인원 설정(1...3)
         
+        print(line)
         print("좌석을 선택해 주세요 (ex.A1 A2 A3)")
         print("    1  2  3  4  5  6  7  8  9  10")
         for (i, pick) in time.pickedSeat.enumerated() {
@@ -69,15 +94,17 @@ while choice != "0" {
         // 2개 이상의 좌석을 입력할 때 구분자 지정
         // 유효성 검사(4) : 입력값 검증(문자+숫자 or list에 있는 목록으로만)
         
+        print(line)
         print("회원님의 휴대전화 번호를 입력해주세요 (ex.010-0000-0000)")
         let phoneNumber = readLine()!
         // 유효성 검사(5) : 입력값 검증(숫자, 구분자 "-")
         
+        print(line)
         print("결제를 진행하시겠습니까? (Y/N)")
         if readLine()! == "Y" {
             movie.getPromotion()
             // (유효성 이후 추가 기능) 지갑 기능, 잔고 있을 때 결제 되고 없으면 못함
-            bookedList.append(Ticket(title: movie.title, timeTable: time, headCount: headCount, seats: pickedSeat, phoneNumber: phoneNumber))
+            bookedList.append(Ticket(title: movie.title, timeTable: time, headCount: headCount, seats: pickedSeat, phoneNumber: phoneNumber))            
             print("예매가 완료되었습니다")
         } else {
             print("결제가 취소되었습니다")
